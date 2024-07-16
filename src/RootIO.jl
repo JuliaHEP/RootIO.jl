8d888c93a2b2f69026b098874958156cccd876c9
Yash Solanki <67216443+yashnator@users.noreply.github.com>
2024-07-16 07:11:15 +0530

    [Chore] Add doc strings to the functions

--- src/RootIO.jl
+++ src/RootIO.jl
@@ -2,16 +2,50 @@ module RootIO
 
 import ROOT, Tables, CxxWrap
 
+"""
+    struct TTree
+
+A struct representing a ROOT TTree with its associated branches and file.
+
+# Fields
+- `_ROOT_ttree`: The ROOT TTree object.
+- `_branch_array`: An array of branches associated with the TTree.
+- `_file`: A pointer to the ROOT file where the TTree is stored.
+"""
 struct TTree
     _ROOT_ttree::ROOT.TTree
     _branch_array
     _file::CxxWrap.CxxWrapCore.CxxPtr{ROOT.TFile}
 end
 
+"""
+    Write(tree::TTree)
+
+Writes a ROOT TTree to the associated ROOT file.
+
+# Arguments
+- `tree::TTree`: The TTree object to be written to the ROOT file.
+"""
 function Write(tree::TTree)
     ROOT.Write(tree._ROOT_ttree)
 end
 
+
+"""
+    _makeTTree(file::CxxWrap.CxxWrapCore.CxxPtr{ROOT.TFile}, name::String, title::String, _branch_types, _branch_names)
+
+Creates a ROOT TTree with specified branches.
+
+# Arguments
+- `file`: A pointer to a ROOT file where the TTree will be stored.
+- `name`: The name of the TTree.
+- `title`: The title of the TTree.
+- `_branch_types`: A collection of types for the branches.
+- `_branch_names`: A collection of names for the branches.
+
+# Returns
+- A RootIO `TTree` object containing the ROOT TTree and its branches.
+"""
 function _makeTTree(file::CxxWrap.CxxWrapCore.CxxPtr{ROOT.TFile}, name::String, title::String, _branch_types, _branch_names)
     _tree = ROOT.TTree(name, title)
     _curr_branch_array = []
@@ -28,8 +62,38 @@ function _makeTTree(file::CxxWrap.CxxWrapCore.CxxPtr{ROOT.TFile}, name::String, 
     return TTree(_tree, _curr_branch_array, file)
 end
 
-function TTree(file::CxxWrap.CxxWrapCore.CxxPtr{ROOT.TFile}, name::String, title::String, data)
+"""
+    TTree(file::CxxWrap.CxxWrapCore.CxxPtr{ROOT.TFile}, name::String, title::String, data)
 
+Creates a new ROOT TTree and fills it with the provided data.
+
+# Arguments
+- `file`: A pointer to a ROOT file where the TTree will be stored.
+- `name`: The name of the TTree.
+- `title`: The title of the TTree.
+- `data`: The data used to define and optionally fill the branches of the TTree. This can be a `DataType` or an instance of a type with fields.
+
+# Example
+```julia
+mutable struct Event
+    x::Float32
+    y::Float32
+    z::Float32
+    v::StdVector{Float32}
+end
+f = ROOT.TFile!Open("data.root", "RECREATE")
+Event()  = Event(0., 0., 0., StdVector{Float32}())
+tree = RootIO.TTree(f, "mytree", "mytreetitle", Event)
+e = Event()
+for i in 1:10
+    e.x, e.y, e.z = rand(3)
+    resize!.([e.v], 5)
+    e.v .= rand(Float32, 5)
+    RootIO.Fill(tree, e)
+end
+````
+"""
+function TTree(file::CxxWrap.CxxWrapCore.CxxPtr{ROOT.TFile}, name::String, title::String, data)
     println("Creating a new TTree")
 
     _branch_types_array = []
@@ -51,12 +115,30 @@ function TTree(file::CxxWrap.CxxWrapCore.CxxPtr{ROOT.TFile}, name::String, title
     return it
 end
 
+"""
+    TTree(file::CxxWrap.CxxWrapCore.CxxPtr{ROOT.TFile}, name::String, title::String; kwargs...)
+
+Creates a new ROOT TTree and fills it with the provided data.
+
+# Arguments
+- `file`: A pointer to a ROOT file where the TTree will be stored.
+- `name`: The name of the TTree.
+- `title`: The title of the TTree.
+- `kwargs...`: Named arguments representing the branches of the TTree. Each named argument is either a data type or an array of data.
+
+# Example
+```julia
+file = ROOT.TFile!Open("example.root", "RECREATE")
+name = "example_tree"
+title = "Example TTree"
+data = (col_float=rand(Float64, 3), col_int=rand(Int32, 3))
+tree = RootIO.TTree(file, name, title; data...)
+"""
 function TTree(file::CxxWrap.CxxWrapCore.CxxPtr{ROOT.TFile}, name::String, title::String; kwargs...)
     println("Creating a new TTree")
 
     _branch_types_array = []
     _branch_names_array = NTuple{length(kwargs), Symbol}(collect(keys(kwargs)))
-
     if isa(kwargs[1], DataType)
         _branch_types_array = NTuple{length(kwargs), DataType}(collect(values(kwargs)))
     else
@@ -84,6 +166,20 @@ function TTree(file::CxxWrap.CxxWrapCore.CxxPtr{ROOT.TFile}, name::String, title
     return it
 end
 
+"""
+    Fill(tree::TTree, data)
+
+Fills a ROOT TTree with the provided data.
+
+# Arguments
+- `tree`: The TTree object to be filled.
+- `data`: The data to fill the TTree with. This can be a table or a row.
+
+# Example
+```julia
+# Assuming `tree` is an existing TTree and `data` is a table or row
+Fill(tree, data)
+"""
 function Fill(tree::TTree, data)
     if Tables.istable(data)
         println("Filling the TTree with a table")
